1241c23919df7ff90bb246dae3d0dc01847ab913
StanislavBondarenko631 <skyritm201287@gmail.com>
2026-09-13 19:27:33 +0300

    add form, add services and types

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,25 @@
 import Section from "../Section/Section";
 import Container from "../Container/Container";
+import Form from "../Form/Form";
+import { getPhotos } from "../../services/photos";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function App() {
+  const handleSearch = async (query: string) => {
+    try {
+      const data = await getPhotos(query);
+      console.log(data);
+    } catch {
+      toast.error("Somthing wrongs ... Try again");
+    }
+  };
   return (
     <>
       <Section>
-        <Container>{/* Компоненти застосунку */}</Container>
-        Home page
+        <Container>
+          <Toaster position="top-center" />
+          <Form onSubmit={handleSearch} />
+        </Container>
       </Section>
     </>
   );
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,9 +3,23 @@ import toast from "react-hot-toast";
 
 import style from "./Form.module.css";
 
-export default function Form() {
+interface FormProps {
+  onSubmit: (query: string) => void;
+}
+
+export default function Form({ onSubmit }: FormProps) {
+  const handleSubmit = (formData: FormData) => {
+    const rawQuery = formData.get("search") as string;
+
+    if (rawQuery.trim() === "") {
+      toast.error("Please enter your search query.");
+      return;
+    }
+
+    onSubmit(rawQuery);
+  };
   return (
-    <form className={style.form}>
+    <form action={handleSubmit} className={style.form}>
       <input
         className={style.input}
         placeholder="What do you want to write?"
